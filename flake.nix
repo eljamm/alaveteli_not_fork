553bb53966afb884f8396529003c46620f824b58
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-03 15:29:37 +0100

    Refactor DevShell

--- flake.nix
+++ flake.nix
@@ -92,7 +92,7 @@
               libzip
               pdftk
               poppler
-              poppler_utils
+              poppler-utils
               tnef
               unrtf
               xapian
@@ -114,171 +114,176 @@
           ];
         };
 
-        devShells =
-          let
-            dbUser = "postgres";
-            dbHost = "localhost";
-            dbPort = 54321;
-            railsPort = "3030"; # to avoid conflict with commonly used 3000
-            toYAML = pkgs.lib.generators.toYAML { };
+        devShells = {
+          # use this one to develop on core alaveteli, without a theme
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [ self.nixosModules.common ];
+          };
 
-            rails_db_conf = {
-              # this config must be overridden in the theme
-              development = {
-                adapter = "postgresql";
-                template = "template_utf8";
-                host = dbHost;
-                port = dbPort;
-                database = "alaveteli_development";
-                username = dbUser;
-                password = "changeme";
-              };
-            };
-            rails_db_conf_file = pkgs.writeText "database.yml" (toYAML rails_db_conf);
-            # ideally, this would load general.yml-example and override its contents
-            # with whatever is passed below
-            alaveteli_config_general = pkgs.writeText "general.yml" (toYAML {
-              OVERRIDE_ALL_PUBLIC_BODY_REQUEST_EMAILS = "publicbody@localhost";
-              # THEME_URLS = [ "https://github.com/mysociety/alavetelitheme.git" ];
-              THEME_URLS = [
-                "https://gitlab.com/madada-team/dada-core.git"
-              ];
-            });
-          in
-          {
-            # commonModules are exposed here so that each devenv can access
-            # the various components and override them
-            commonModules = {
-              packages =
-                self.packagesForAlaveteli.${system}.running { } ++ self.packagesForAlaveteli.${system}.developing;
+          # use this env to develop with some custom theme
+          # This lives here as creating a dev env from a separate folder than
+          # Rails.root is tricky. The theme folder must be linked from, or copied to,
+          # ./lib/themes
+          # Start it with: nix develop --no-pure-eval .#devWithTheme
+          devWithTheme = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              (
+                self.nixosModules.common
+                // {
+                  # enterShell = self.devShells.${system}.default.enterShell
+                  enterShell = self.devShells.${system}.enterShell + "echo Using theme";
+                  env = self.devShells.${system}.env // {
+                    FOOENV = "themeON";
+                  };
+                }
+              )
+            ];
+          };
+        };
+      }
+    )
+    // flake-utils.lib.eachDefaultSystemPassThrough (system: {
+      # commonModules are exposed here so that each devenv can access
+      # the various components and override them
+      nixosModules.common =
+        {
+          pkgs,
+          ...
+        }:
+        let
+          dbUser = "postgres";
+          dbHost = "localhost";
+          dbPort = 54321;
+          railsPort = "3030"; # to avoid conflict with commonly used 3000
+          toYAML = pkgs.lib.generators.toYAML { };
 
-              enterShell = ''
-                export GIT_DIR=$DEVENV_ROOT/.git
-                export GIT_WORK_TREE=$DEVENV_ROOT
-                git submodule update --init
-                # TODO: make sure we use local file storage by default in dev env
-                cp config/storage.yml-example config/storage.yml
-                rm -f config/general.yml
-                ln -s "${alaveteli_config_general}" config/general.yml
-                # use the madada config file
-                # ln -s ../../dada-core/config/general_dada.yml config/general.yml
-                rm -f config/database.yml
-                ln -s "${rails_db_conf_file}" config/database.yml
-                #
-                # The env is now ready
-                #
-                figlet -f roman -w 90 Alaveteli
-                echo "Alaveteli core dev env ready"
-                echo "The services you need (postgres, redis, rails server...) can be started with 'devenv up'"
-                echo "(keep them running in a separate terminal)"
-                echo "once devenv up is ready, alaveteli will be running at http://localhost:${railsPort}/"
-                echo "useful commands:"
-                echo "rails c (no path, just this!)"
-                echo "Outgoing emails are here: http://localhost:8025"
-              '';
-
-              # this is required to build the pg gem on linux
-              # TODO: can we move this to gemConfig instead? we don't need
-              # this env var once the gem is built
-              env = {
-                LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
-                  pkgs.krb5
-                  pkgs.openldap
-                ];
-              };
-
-              processes = {
-                # run migrations once postgres is started
-                migrate = {
-                  exec = "rails db:migrate && rails db:seed";
-                  process-compose.depends_on.postgres.condition = "process_healthy";
-                };
-                init_xapian = {
-                  exec = ''
-                    cd $DEVENV_ROOT
-                    echo "initing xapian (todo, needs to be run in the alaveteli root folder)"
-                    # trap 'kill -KILL $(jobs -p); wait; exit 0;' SIGTERM
-                    # TODO: patch task code to allow configuring the db path outside
-                    # of rails root dir
-                    # RAILS_ENV=development rake xapian:create_index
-                    # wait
-                  '';
-                  process-compose.depends_on.migrate.condition = "process_completed_successfully";
-                };
-                # start the dev web server after migrations
-                web = {
-                  exec = "rails server -p ${railsPort}";
-                  process-compose.depends_on.init_xapian.condition = "process_completed_successfully";
-                };
-              };
-
-              scripts.db = {
-                description = "Open the database in psql";
-                exec = ''
-                  psql -U ${dbUser} -h ${dbHost} -p ${toString dbPort} ${rails_db_conf.development.database}
-                '';
-                packages = [ pkgs.postgresql_16 ];
-              };
-
-              services.postgres = {
-                enable = true;
-                package = pkgs.postgresql_16;
-                initialDatabases = [
-                  {
-                    name = "alaveteli_test";
-                    user = dbUser;
-                  }
-                  {
-                    name = "alaveteli_development";
-                    user = dbUser;
-                  }
-                  {
-                    name = "alaveteli_production";
-                    user = dbUser;
-                  }
-                ];
-                initialScript = "CREATE ROLE postgres SUPERUSER; ALTER ROLE postgres WITH LOGIN;";
-                listen_addresses = dbHost;
-                port = dbPort;
-                extensions = extensions: [ ];
-              };
-              # alaveteli knows to send email to port 1025 in dev
-              # which is the default for mailpit
-              services.mailpit = {
-                enable = true;
-              };
-              services.redis = {
-                enable = true;
-              };
-            };
-
-            # use this one to develop on core alaveteli, without a theme
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [ self.devShells.${system}.commonModules ];
-            };
-
-            # use this env to develop with some custom theme
-            # This lives here as creating a dev env from a separate folder than
-            # Rails.root is tricky. The theme folder must be linked from, or copied to,
-            # ./lib/themes
-            # Start it with: nix develop --no-pure-eval .#devWithTheme
-            devWithTheme = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                (
-                  self.devShells.${system}.commonModules
-                  // {
-                    # enterShell = self.devShells.${system}.default.enterShell
-                    enterShell = self.devShells.${system}.commonModules.enterShell + "echo Using theme";
-                    env = self.devShells.${system}.commonModules.env // {
-                      FOOENV = "themeON";
-                    };
-                  }
-                )
-              ];
+          rails_db_conf = {
+            # this config must be overridden in the theme
+            development = {
+              adapter = "postgresql";
+              template = "template_utf8";
+              host = dbHost;
+              port = dbPort;
+              database = "alaveteli_development";
+              username = dbUser;
+              password = "changeme";
             };
           };
-      }
-    );
+          rails_db_conf_file = pkgs.writeText "database.yml" (toYAML rails_db_conf);
+          # ideally, this would load general.yml-example and override its contents
+          # with whatever is passed below
+          alaveteli_config_general = pkgs.writeText "general.yml" (toYAML {
+            OVERRIDE_ALL_PUBLIC_BODY_REQUEST_EMAILS = "publicbody@localhost";
+            # THEME_URLS = [ "https://github.com/mysociety/alavetelitheme.git" ];
+            THEME_URLS = [
+              "https://gitlab.com/madada-team/dada-core.git"
+            ];
+          });
+        in
+        {
+          packages =
+            self.packagesForAlaveteli.${system}.running { } ++ self.packagesForAlaveteli.${system}.developing;
+
+          enterShell = ''
+            export GIT_DIR=$DEVENV_ROOT/.git
+            export GIT_WORK_TREE=$DEVENV_ROOT
+            git submodule update --init
+            # TODO: make sure we use local file storage by default in dev env
+            cp config/storage.yml-example config/storage.yml
+            rm -f config/general.yml
+            ln -s "${alaveteli_config_general}" config/general.yml
+            # use the madada config file
+            # ln -s ../../dada-core/config/general_dada.yml config/general.yml
+            rm -f config/database.yml
+            ln -s "${rails_db_conf_file}" config/database.yml
+            #
+            # The env is now ready
+            #
+            figlet -f roman -w 90 Alaveteli
+            echo "Alaveteli core dev env ready"
+            echo "The services you need (postgres, redis, rails server...) can be started with 'devenv up'"
+            echo "(keep them running in a separate terminal)"
+            echo "once devenv up is ready, alaveteli will be running at http://localhost:${railsPort}/"
+            echo "useful commands:"
+            echo "rails c (no path, just this!)"
+            echo "Outgoing emails are here: http://localhost:8025"
+          '';
+
+          # this is required to build the pg gem on linux
+          # TODO: can we move this to gemConfig instead? we don't need
+          # this env var once the gem is built
+          env = {
+            LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
+              pkgs.krb5
+              pkgs.openldap
+            ];
+          };
+
+          processes = {
+            # run migrations once postgres is started
+            migrate = {
+              exec = "rails db:migrate && rails db:seed";
+              process-compose.depends_on.postgres.condition = "process_healthy";
+            };
+            init_xapian = {
+              exec = ''
+                cd $DEVENV_ROOT
+                echo "initing xapian (todo, needs to be run in the alaveteli root folder)"
+                # trap 'kill -KILL $(jobs -p); wait; exit 0;' SIGTERM
+                # TODO: patch task code to allow configuring the db path outside
+                # of rails root dir
+                # RAILS_ENV=development rake xapian:create_index
+                # wait
+              '';
+              process-compose.depends_on.migrate.condition = "process_completed_successfully";
+            };
+            # start the dev web server after migrations
+            web = {
+              exec = "rails server -p ${railsPort}";
+              process-compose.depends_on.init_xapian.condition = "process_completed_successfully";
+            };
+          };
+
+          scripts.db = {
+            description = "Open the database in psql";
+            exec = ''
+              psql -U ${dbUser} -h ${dbHost} -p ${toString dbPort} ${rails_db_conf.development.database}
+            '';
+            packages = [ pkgs.postgresql_16 ];
+          };
+
+          services.postgres = {
+            enable = true;
+            package = pkgs.postgresql_16;
+            initialDatabases = [
+              {
+                name = "alaveteli_test";
+                user = dbUser;
+              }
+              {
+                name = "alaveteli_development";
+                user = dbUser;
+              }
+              {
+                name = "alaveteli_production";
+                user = dbUser;
+              }
+            ];
+            initialScript = "CREATE ROLE postgres SUPERUSER; ALTER ROLE postgres WITH LOGIN;";
+            listen_addresses = dbHost;
+            port = dbPort;
+            extensions = extensions: [ ];
+          };
+          # alaveteli knows to send email to port 1025 in dev
+          # which is the default for mailpit
+          services.mailpit = {
+            enable = true;
+          };
+          services.redis = {
+            enable = true;
+          };
+        };
+    });
 }
